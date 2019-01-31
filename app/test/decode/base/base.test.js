cb74788a37c7c2963b810482c4f64e5c619ab3f6
zer0i3 <zer0i3@aliyun.com>
2019-01-31 09:31:44 +0800

    add mocha test

---
+++ app/test/decode/base/base.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+import {baseEncode, baseDecode} from '../../../src/components/DecodeComponent/DecodeItem/base/base'
+
+describe('#base.js',() => {
+    
+    describe('#encode()',() => {
+       it("baseEncode('nekoneko','base64') should equal bmVrb25la28=",() => {
+            assert.strictEqual(baseEncode('nekoneko','base64'),'bmVrb25la28=');
+       });
+    });
+
+    describe('#decode()',() => {
+        it("baseDecode('bmVrb25la28=','base64') should equal nekoneko",() => {
+            assert.strictEqual(baseDecode('bmVrb25la28=','base64'),'nekoneko');
+        });
+    });
+});
