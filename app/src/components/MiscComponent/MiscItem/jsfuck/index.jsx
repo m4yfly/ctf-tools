9a9f90a758f9131328825532bb8ccb4b8f6ea7f3
zer0i3 <zer0i3@aliyun.com>
2019-02-22 09:56:02 +0800

    add jsfuck

--- app/src/components/MiscComponent/MiscItem/jsfuck/index.jsx
+++ app/src/components/MiscComponent/MiscItem/jsfuck/index.jsx
@@ -1,10 +1,12 @@
 import React, {Component}  from 'react'
+import CommonDecodeItem from '../../../Common/CommonDecodeItem'
+import {jfencode,jfdecode} from './jsfuck'
 
 
 export default class JSFuckItem extends Component {
     render() {
         return (
-            <div>JSFuck</div>
+            <CommonDecodeItem handleEncode={jfencode} handleDecode={jfdecode} header="jsfuck" />
         )
     }
 }
